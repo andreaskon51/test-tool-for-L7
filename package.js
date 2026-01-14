8d4bd3af1a9f68a8502fa578f345c0b1920a996f
Crude <50704825+andreaskon51@users.noreply.github.com>
2026-01-14 04:32:36 +0200

    Create package.json for stresser-js project
    Initial package.json file for stresser-js project.

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "stresser-js",
+  "version": "4.5.0",
+  "description": "Advanced HTTP Stress Tester - JA3 + Browser Emulation",
+  "main": "stresser.js",
+  "scripts": {
+    "start": "node stresser.js",
+    "test": "node stresser.js"
+  },
+  "keywords": ["stress", "testing", "http", "performance", "ja3", "tls", "fingerprint"],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "axios": "^1.6.5",
+    "https-proxy-agent": "^7.0.2",
+    "http-proxy-agent": "^7.0.0"
+  }
+}
